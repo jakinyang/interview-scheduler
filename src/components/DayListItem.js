585f5e93051d1d8bb7a0125f4082ee07f4f12b3a
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-28 23:15:50 -0800

    Completes DayList component and passes props to children

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,10 +7,19 @@ export default function DayListItem(props) {
     'day-list__item--selected': props.selected,
     'day-list__item--full': !props.spots
   })
+  const formatSpots = (spotsNum) => {
+    return spotsNum ?
+      spotsNum === 1 ?
+        `${spotsNum} spot remaining`
+        :
+        `${spotsNum} spots remaining`
+      :
+      'no spots remaining'
+  }
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li key={props.key} className={itemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
